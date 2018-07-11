f9a1fba850c6d3eb1d72d82856795860cf27ab0b
oliviakris24 <okfishy@gmail.com>
2018-07-11 16:30:37 +0000

    automatically backed up by learn

---
+++ curfew_checker.rb
@@ -0,0 +1,40 @@
+def curfew_time (time)
+  if time >= 11
+    puts "You're in trouble"
+  else time < 11
+    puts "Keep having fun"
+  end 
+end 
+
+curfew_time (10)
+
+def fifth_year (time1)
+  if time1 > 11
+    puts "You're in trouble"
+  elsif time1 == 11
+    puts "Time to apparate"
+  else time1 < 11
+    puts "You can keep having fun"
+  end 
+end 
+
+fifth_year (11)
+  
+def deluxe_curfew 
+time2 = 10  
+curfew = 11
+  if time2 > 11
+    puts "You're in trouble"
+  elsif time2 == 11
+    puts "Time to apparate"
+  else time2 < 11
+    puts "You have #{curfew-time2} hour(s) left. Keep having fun until your curfew!"
+  end 
+end 
+
+deluxe_curfew 
+
+
+    
+   
+  
